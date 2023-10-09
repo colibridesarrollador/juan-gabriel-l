e48bd01fc9621ef2b52073d7eaaae6425f362fc1
JuanG <piedradesales@gmail.com>
2023-10-09 22:05:09 +0200

    tercer_commit

--- src/app/tabs/tab1/tab1.page.ts
+++ src/app/tabs/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { ToastController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,25 +8,34 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
   items: any[] = [
-    { text: 'Mario Bross', checked: false, image:"/assets/mario.png" },
-    { text: 'Crash Bandicoot', checked: false, image:"/assets/crash.png" },
-    { text: 'Megaman', checked: false, image:"/assets/megaman.jpg" },
+    { text: 'Mario Bross', checked: false, image: "/assets/mario.png", mostrarTarjeta: false, imagenTarjeta: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Super_Mario_Bros._Logo.svg/800px-Super_Mario_Bros._Logo.svg.png", descripcion: "Mario Bross es un personaje famoso de videojuegos.", url: 'https://www.minijuegos.com/juego/super-mario-bros' },
+    { text: 'Crash Bandicoot', checked: false, image: "/assets/crash.png", mostrarTarjeta: false, imagenTarjeta: "https://media.vandal.net/m/1420/crash-bandicoot-201610518540_1.jpg", descripcion: "Crash Bandicoot es un marsupial aventurero.", url: 'https://www.minijuegos.com/juego/crash-bandicoot' },
+    { text: 'Megaman', checked: false, image: "/assets/megaman.jpg", mostrarTarjeta: false, imagenTarjeta: "https://lostmediawiki.com/w/images/3/3c/Mega_Man_II_Lite_Startup.png", descripcion: "Megaman es un héroe que puede copiar poderes.", url: 'https://www.minijuegos.com/juego/mega-man-rpg-2' },
   ];
 
-  constructor(public toastController: ToastController) {}
+  constructor(public toastController: ToastController, public navCtrl: NavController) {}
 
-  showToast(item: any) {
+  mostrarTarjeta(item: any) {
     if (item.checked) {
-      this.presentToast(item.text);
+      item.mostrarTarjeta = true;
+      this.presentarToast(`Seleccionaste: ${item.text}`);
+    } else {
+      item.mostrarTarjeta = false;
     }
   }
 
-  async presentToast(message: string) {
+  async presentarToast(mensaje: string) {
     const toast = await this.toastController.create({
-      message: `Seleccionaste: ${message}`,
+      message: mensaje,
       duration: 2000,
       position: 'top',
     });
     toast.present();
+  }
+
+  // Función para abrir un URL
+  irAURL(url: string) {
+    // Abre el URL en una nueva ventana del navegador
+    window.open(url, '_blank');
   }
 }
