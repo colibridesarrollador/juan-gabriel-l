6659d43f66af9d996b1de112fdd8d809032aad6d
JuanG <piedradesales@gmail.com>
2023-10-11 12:11:49 +0200

    ultimo-commit

--- src/app/tabs/tab1/tab1.page.ts
+++ src/app/tabs/tab1/tab1.page.ts
@@ -8,9 +8,9 @@ import { ToastController, NavController } from '@ionic/angular';
 })
 export class Tab1Page {
   items: any[] = [
-    { text: 'Mario Bross', checked: false, image: "/assets/mario.png", mostrarTarjeta: false, imagenTarjeta: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Super_Mario_Bros._Logo.svg/800px-Super_Mario_Bros._Logo.svg.png", descripcion: "Mario Bross es un personaje famoso de videojuegos.", url: 'https://www.minijuegos.com/juego/super-mario-bros' },
-    { text: 'Crash Bandicoot', checked: false, image: "/assets/crash.png", mostrarTarjeta: false, imagenTarjeta: "https://media.vandal.net/m/1420/crash-bandicoot-201610518540_1.jpg", descripcion: "Crash Bandicoot es un marsupial aventurero.", url: 'https://www.minijuegos.com/juego/crash-bandicoot' },
-    { text: 'Megaman', checked: false, image: "/assets/megaman.jpg", mostrarTarjeta: false, imagenTarjeta: "https://lostmediawiki.com/w/images/3/3c/Mega_Man_II_Lite_Startup.png", descripcion: "Megaman es un héroe que puede copiar poderes.", url: 'https://www.minijuegos.com/juego/mega-man-rpg-2' },
+    { text: 'Mario Bross', checked: false, image: "assets/mario.png", mostrarTarjeta: false, imagenTarjeta: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Super_Mario_Bros._Logo.svg/800px-Super_Mario_Bros._Logo.svg.png", descripcion: "Mario Bross es un personaje famoso de videojuegos.", url: 'https://www.minijuegos.com/juego/super-mario-bros' },
+    { text: 'Crash Bandicoot', checked: false, image: "assets/crash.png", mostrarTarjeta: false, imagenTarjeta: "https://media.vandal.net/m/1420/crash-bandicoot-201610518540_1.jpg", descripcion: "Crash Bandicoot es un marsupial aventurero.", url: 'https://www.minijuegos.com/juego/crash-bandicoot' },
+    { text: 'Megaman', checked: false, image: "assets/megaman.jpg", mostrarTarjeta: false, imagenTarjeta: "https://lostmediawiki.com/w/images/3/3c/Mega_Man_II_Lite_Startup.png", descripcion: "Megaman es un héroe que puede copiar poderes.", url: 'https://www.minijuegos.com/juego/mega-man-rpg-2' },
   ];
 
   constructor(public toastController: ToastController, public navCtrl: NavController) {}
@@ -31,11 +31,5 @@ export class Tab1Page {
       position: 'top',
     });
     toast.present();
-  }
-
-  // Función para abrir un URL
-  irAURL(url: string) {
-    // Abre el URL en una nueva ventana del navegador
-    window.open(url, '_blank');
   }
 }
--- src/app/tabs/tab3/tab3.page.ts
+++ src/app/tabs/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-tab3',
   templateUrl: './tab3.page.html',
@@ -11,16 +12,21 @@ export class Tab3Page {
   itemSeleccionado2: boolean = false;
   itemSeleccionado3: boolean = false;
 
-  constructor(public toastController: ToastController) {}
+  constructor(
+    public toastController: ToastController,
+  
+  ) {}
 
   async showToast(message: string, itemSeleccionado: boolean) {
     if (itemSeleccionado) {
       const toast = await this.toastController.create({
         message: message,
-        duration: 2000, // Duración del mensaje en milisegundos
-        position: 'top', // Posición del mensaje (top, bottom, middle)
+        duration: 2000,
+        position: 'top',
       });
       toast.present();
     }
   }
+
 }
+
